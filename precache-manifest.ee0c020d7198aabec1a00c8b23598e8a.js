7beac9bb2ac9b0ae3fdff161e57cdac0e5f69d63
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2020-03-29 17:50:58 +0100

    Updates

---
+++ precache-manifest.ee0c020d7198aabec1a00c8b23598e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bee00af32d841e2719229b3d54878d",
+    "url": "/wotvffbe_tools/index.html"
+  },
+  {
+    "revision": "0a6887e990c6a45f69f3",
+    "url": "/wotvffbe_tools/static/css/main.8bdc1e70.chunk.css"
+  },
+  {
+    "revision": "e915fe20129188794013",
+    "url": "/wotvffbe_tools/static/js/2.4427d23e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wotvffbe_tools/static/js/2.4427d23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6887e990c6a45f69f3",
+    "url": "/wotvffbe_tools/static/js/main.2002fe7d.chunk.js"
+  },
+  {
+    "revision": "83130b54a07b6ee4bb8e",
+    "url": "/wotvffbe_tools/static/js/runtime-main.53279aae.js"
+  }
+]);
